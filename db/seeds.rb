3c47c3210713262fa99be9164a69b0cc1ef87adf
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-19 10:40:39 +0200

    seed of restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+resto = Restaurant.create!({
+  name: "Le Dindon en Laisse",
+  address: "18 Rue Beautreillis, 75004 Paris, France"
+})
+puts "#{resto.name} created"
+resto = Restaurant.create!({
+  name: "Neuf et Voisins",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+})
+puts "#{resto.name} created"
