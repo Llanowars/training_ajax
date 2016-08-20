636157232c9840ef071c5fcbea030469bcedb5c1
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-20 10:34:31 +0200

    inlined review

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'restaurants#index'
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 
 end
