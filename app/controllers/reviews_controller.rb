7f959c3c2a0a3eecafc2413508322b32cdbf544a
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-16 15:53:55 +0200

    checkpoint

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
+  
+  before_action :set_restaurant, only: [:create, :destroy]
+
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -25,5 +27,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
